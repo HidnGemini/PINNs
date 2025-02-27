1a0e5a05cb14e69c038951f98d74aba6bdc09a76
Branson Jones <tenmilejones@gmail.com>
2025-02-26 20:50:01 -0800

    Refactored animate_both() into more generalized animate_together(), which can either graph 2 PINNs against each other or graph one against the reference solution

--- animate_ice_PINN.py
+++ animate_ice_PINN.py
@@ -124,39 +124,68 @@ def animate_IcePINN(model_name, index, frame_interval = 50):
     plt.show()
 
 
-def animate_both(model_name, index, frame_interval = 50):
+def animate_together(model1_name, model2_name, index, frame_interval = 50):
     """
     Animates PINN model output superimposed on reference solution.
 
     Args:
-        model_name: String name of folder model is stored in
+        model1_name: String name of folder model is stored in, or reference solution if None.
+        model2_name: String name of folder model is stored in, or reference solution if None.
         index: 0 for Ntot, 1 for Nqll, 2 for N-ice
         frame_interval: time in ms that each frame should remain on screen
     """
-    loaded_model = ip.load_IcePINN(model_name)
-    loaded_model.eval()
+    # Yes I know there is some egregious code duplication and the next 20 lines should
+    # go into a helper method. I realized once it was done and I'm not changing it.
 
-    # Get predictions from the network using test data
-    pred = ip.enforced_model(TEST_SET, loaded_model)
-    Ntot_pred = pred[:, 0]
-    Nqll_pred = pred[:, 1]
-    Nice_pred = Ntot_pred - Nqll_pred
+    model1_solution = "foo"
+    model2_solution = "bar"
+    
+    if model1_name is None:
+        # Generate reference solution
+        model1_solution = refsol.generate_reference_solution(runtime=RUNTIME, num_steps=NUM_T_STEPS)
+        model1_name = "Reference Solution"
+    else:
+        model1 = ip.load_IcePINN(model1_name)
+        model1.eval()
 
-    # Stack predictions to match expected output shape
-    network_solution = torch.stack(
-        [Ntot_pred, Nqll_pred, Nice_pred], 
-        axis=0).reshape(3, NUM_T_STEPS, int(len(TEST_SET)/NUM_T_STEPS)
-            ).cpu().detach().numpy() # Animation breaks on GPU; move to CPU
+        # Get predictions from model1 using test data
+        pred1 = ip.enforced_model(TEST_SET, model1)
+        Ntot_pred1 = pred1[:, 0]
+        Nqll_pred1 = pred1[:, 1]
+        Nice_pred1 = Ntot_pred1 - Nqll_pred1
 
-    # Generate reference solution
-    REFERENCE_SOLUTION = refsol.generate_reference_solution(runtime=RUNTIME, num_steps=NUM_T_STEPS)
+        # Stack predictions to match expected output shape
+        model1_solution = torch.stack(
+            [Ntot_pred1, Nqll_pred1, Nice_pred1], 
+            axis=0).reshape(3, NUM_T_STEPS, int(len(TEST_SET)/NUM_T_STEPS)
+                ).cpu().detach().numpy() # Animation breaks on GPU; move to CPU
+    
+    if model2_name is None:
+        # Generate reference solution
+        model2_solution = refsol.generate_reference_solution(runtime=RUNTIME, num_steps=NUM_T_STEPS)
+        model2_name = "Reference Solution"
+    else:
+        model2 = ip.load_IcePINN(model2_name)
+        model2.eval()
+
+        # Get predictions from model2 using test data
+        pred2 = ip.enforced_model(TEST_SET, model2)
+        Ntot_pred2 = pred2[:, 0]
+        Nqll_pred2 = pred2[:, 1]
+        Nice_pred2 = Ntot_pred2 - Nqll_pred2
+
+        # Stack predictions to match expected output shape
+        model2_solution = torch.stack(
+            [Ntot_pred2, Nqll_pred2, Nice_pred2], 
+            axis=0).reshape(3, NUM_T_STEPS, int(len(TEST_SET)/NUM_T_STEPS)
+                ).cpu().detach().numpy() # Animation breaks on GPU; move to CPU
 
     # set up graph
     fig, ax = plt.subplots()
     ax.set(xlim=[-L, L], xlabel="x (micrometers)", ylabel="Number of Ice Layers", title="Predicted "+TITLE_DICT[index]) # set axes
 
-    lines = ax.plot(X_QLC, REFERENCE_SOLUTION[index][0], 'b', linewidth=1.0, label="Reference Value", zorder=2)
-    lines.append(ax.plot(X_QLC, network_solution[index][0], 'r', linewidth=1.0, label="Network Prediction", zorder=0)[0])
+    lines = ax.plot(X_QLC, model1_solution[index][0], 'b', linewidth=1.0, label=model1_name, zorder=2)
+    lines.append(ax.plot(X_QLC, model2_solution[index][0], 'r', linewidth=1.0, label=model2_name, zorder=0)[0])
     ax.legend()
 
     # list of frames to iterate through (a list of unique times)
@@ -166,8 +195,8 @@ def animate_both(model_name, index, frame_interval = 50):
     def update(frame):
 
         # select y values that correspond to the frame in question
-        ref_y = REFERENCE_SOLUTION[index][frame]
-        net_y = network_solution[index][frame]
+        ref_y = model1_solution[index][frame]
+        net_y = model2_solution[index][frame]
         
         # Update y-axis limits to fit the data
         ax.set_ylim(np.min([np.min(net_y), np.min(ref_y)]), np.max([np.max(net_y), np.max(ref_y)]))
